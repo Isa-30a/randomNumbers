72f669f54245670698342c7ff90b30af1a998b66
Luigi Salcedo <csalcedoe@unicartagena.edu.co>
2024-09-22 11:28:23 -0500

    agregada prueba de la varianza

--- pruebas/estadisticas/estadisticas.go
+++ pruebas/estadisticas/estadisticas.go
@@ -3,6 +3,8 @@ package estadisticas
 import (
 	"fmt"
 	"math"
+
+	"gonum.org/v1/gonum/stat/distuv"
 )
 
 func response(b bool) string {
@@ -12,13 +14,19 @@ func response(b bool) string {
 	return "No"
 }
 
-func MediaTest(data []float64) {
+func calcMedia(data []float64) float64 {
 	media := 0.0
 	n := float64(len(data))
 	for _, n := range data {
 		media += n
 	}
 	media /= n
+	return media
+}
+
+func MediaTest(data []float64) {
+	media := calcMedia(data)
+	n := float64(len(data))
 	fmt.Println("\nMedia de los datos:", media)
 	li := 0.5 - (1.96)/math.Sqrt(12*n)
 	ls := 0.5 + (1.96)/math.Sqrt(12*n)
@@ -26,5 +34,25 @@ func MediaTest(data []float64) {
 
 	fmt.Println("Ho = El conjunto tiene un media de 0.5")
 	Ho := li <= media && media <= ls
+	fmt.Printf("¿Se cumple Ho? Respuesta: %s\n", response(Ho))
+}
+
+func VarianzaTest(data []float64) {
+	n := float64(len(data))
+	media := calcMedia(data)
+	varianza := func() float64 {
+		sum := 0.0
+		for _, v := range data {
+			sum += math.Pow((v - media), 2)
+		}
+		return sum / (n - 1)
+	}()
+
+	fmt.Println("Varianza:", varianza)
+	ls := distuv.ChiSquared{K: float64(n - 1)}.Quantile(1-0.05/2) / (12 * (n - 1))
+	li := distuv.ChiSquared{K: float64(n - 1)}.Quantile(0.05/2) / (12 * (n - 1))
+	fmt.Printf("Limites: [%.5f, %.5f]\n", li, ls)
+	fmt.Println("Ho = El conjunto tiene un varianza de 1/12")
+	Ho := li <= varianza && varianza <= ls
 	fmt.Printf("¿Se cumple Ho? Respuesta: %s\n", response(Ho))
 }
--- pruebas/main.go
+++ pruebas/main.go
@@ -14,15 +14,16 @@ func main() {
 	var data []float64
 	loaded := false
 	filename := ""
-	for opc != 6 {
+	for opc != 7 {
 		fmt.Println("\nBienvenido al módulo de pruebas de números pseudo aleatorios")
 		fmt.Println("\n[1]. Cargar datos desde un archivo")
 		fmt.Println("[2]. Ejecutar prueba de Poker")
 		fmt.Println("[3]. Ejecutar prueba de intervalos")
 		fmt.Println("[4]. Ejecutar prueba de Kolgomorov Smirnov")
 		fmt.Println("[5]. Ejecutar prueba de la media")
+		fmt.Println("[6]. Ejecutar prueba de la varianza")
 		fmt.Println()
-		fmt.Println("[6]. Salir")
+		fmt.Println("[7]. Salir")
 		fmt.Println()
 		fmt.Print("Seleccionar: ")
 		fmt.Scan(&opc)
@@ -62,6 +63,12 @@ func main() {
 				continue
 			}
 			estadisticas.MediaTest(data)
+		case 6:
+			if !loaded {
+				fmt.Println("\nNo se han cargado datos")
+				continue
+			}
+			estadisticas.VarianzaTest(data)
 		}
 	}
 }
